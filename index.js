4e4596d815916eff494fc09242c914c05ce195c2
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2016-02-23 00:41:07 -0600

    constants file try to auto-locate ILMerge

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
-var through = require('through2');
+var through = require('through2'),
+  _ = require('lodash'),
+  gutil = require('gulp-util'),
+  constants = require('./lib/constants');
 
 module.exports = function(options) {
+  var options = _.cloneDeep(_.extend(constants.DEFAULTS, options));
+  gutil.log(options);
+
   var stream = through.obj(function(file, enc, callback) {
     var self = this;
     self.push(file);

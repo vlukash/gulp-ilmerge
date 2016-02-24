df5426e49cf9af07938234bd7f1db3392c568587
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2016-02-23 23:43:56 -0600

    get files list from pipeline

--- index.js
+++ index.js
@@ -2,16 +2,35 @@
 var through = require('through2'),
   _ = require('lodash'),
   gutil = require('gulp-util'),
-  constants = require('./lib/constants');
+  path = require('path'),
+  constants = require('./lib/constants'),
+  ilmerge = require('./lib/ilmerge-runner');
+
+var fileList = [];
 
 module.exports = function(options) {
   var options = _.cloneDeep(_.extend(constants.DEFAULTS, options));
   gutil.log(options);
 
-  var stream = through.obj(function(file, enc, callback) {
-    var self = this;
-    self.push(file);
-    return callback();
+  return through.obj(function(file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+
+    if (file.isStream()) {
+      cb(new gutil.PluginError(constants.PLUGIN_NAME, 'Streams not supported'));
+      return;
+    }
+
+    fileList.push(path.relative(process.cwd(), file.path));
+
+    this.push(file);
+    cb();
+
+  },
+  function(cb) {
+    gutil.log(fileList);
+    cb();
   });
-  return stream;
 };

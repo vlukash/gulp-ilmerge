d0e1cc0f1d1835c7ddf722f921e4a268c396f39b
Volodymyr Lukashevych <v.lukashevych@gmail.com>
2016-02-25 22:33:58 -0600

    fix for command-builder tests

--- test/command-builder.js
+++ test/command-builder.js
@@ -8,14 +8,20 @@ require('mocha');
 var commandBuilder = require('../lib/command-builder');
 
 describe('# command-builder', function () {
-  describe('# createCommand(options, files)', function () {
-    it('should build ILMerge command with default options', function () {
+
+  describe('# getArgs(options)', function () {
+    it('should build arguments with default options', function () {
+      var result = commandBuilder.getArgs(constants.DEFAULTS);
+      expect(result).to.equal('/out:"merged.dll" /targetplatform:"v4,C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319" /target:dll');
+    });
+  });
+
+  describe('# getFilesListParam(files)', function () {
+    it('should build files list from array', function () {
       var filrsList = ["primary.dll", "secondary1.dll", "secondary2.dll"]
 
-      var result = commandBuilder.createCommand(constants.DEFAULTS, filrsList);
-      expect(result).to.equal('"C:\\Program Files\\Microsoft\\ILMerge\\ILMerge.exe" ' +
-          '/out:"merged.dll" /targetplatform:"v4,C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319" ' +
-          '/target:dll "primary.dll" "secondary1.dll" "secondary2.dll"');
+      var result = commandBuilder.getFilesListParam(filrsList);
+      expect(result).to.equal('"primary.dll" "secondary1.dll" "secondary2.dll"');
     });
   });
 });
